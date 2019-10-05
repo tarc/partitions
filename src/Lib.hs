6c9b7d0d1a2093c8b7c386b08d7ea0a904612eed
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2019-10-05 05:31:02 -0300

    Tentative implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,5 @@ module Lib
 
 partition :: Word -> [Word] -> [[Word]]
 partition _ [] = []
+partition n (p:ps) = if n < p then (partition n ps)
+                     else [ p:x | x <- (partition (n-p) ps) ] ++ (partition n ps)
