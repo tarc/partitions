b176a460fa895e24bc66ee930b44db6b175e38a3
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2019-10-05 04:52:47 -0300

    Adding test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain (testGroup "Our Library Tests" [mainNumberLessThanSmallestPartition])
+
+mainNumberLessThanSmallestPartition :: TestTree
+mainNumberLessThanSmallestPartition = testCase "Testing "
+  (assertEqual "There's no possible partition for such small number" [] (partition 3))
