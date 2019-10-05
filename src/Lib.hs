6423ba57b46d368c02b751dfefaa59e74fabc786
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2019-10-05 06:32:22 -0300

    Validating algo through partition counting sequence A000041 (in the OEIS)

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,15 @@
 module Lib
-    ( partitions
+    ( partitions ,
+      partition
     ) where
 
 partitions :: Word -> [Word] -> [[Word]]
 partitions _ [] = []
-partitions n (p:ps) = if n < p then (partitions n ps)
+partitions n (p:ps) = if n == 0 && p == 0 then [[0]]
+                     else if p == 0 then (partitions n ps)
+                     else if n < p then (partitions n ps)
                      else if n == p then [ [n] ] ++ (partitions n ps)
                      else [ p:x | x <- (partitions (n-p) $ p:ps) ] ++ (partitions n ps)
+
+partition :: Word -> Word
+partition n = fromIntegral $ length $ partitions n [0..n]
--- test/Spec.hs
+++ test/Spec.hs
@@ -13,7 +13,8 @@ main = do
       mainNumberLessThanSmallestPartition ,
       singlePartition ,
       singlePartition2 ,
-      twoPartitions
+      twoPartitions ,
+      validatePartitionCount
     ])
 
 noPartitionFromEmptySet :: TestTree
@@ -40,3 +41,8 @@ twoPartitions :: TestTree
 twoPartitions = testCase "Testing small value"
   (assertEqual "There's no possible partition for such small number"
   [[3,1],[1,1,1,1]] (partitions 4 [3,1]))
+
+validatePartitionCount :: TestTree
+validatePartitionCount = testCase ""
+  (assertEqual ""
+  [partition n | n <- [0..30]] [1,1,2,3,5,7,11,15,22,30,42,56,77,101,135,176,231,297,385,490,627,792,1002,1255,1575,1958,2436,3010,3718,4565,5604] )
