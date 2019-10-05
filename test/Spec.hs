449b4be76b5df4449de7d8ff8846552e8d6cfe10
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2019-10-05 05:08:08 -0300

    Add failing test for number smaller than minimum value in set

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,9 +7,18 @@ import Lib
 
 main :: IO ()
 main = do
-  defaultMain (testGroup "Our Library Tests" [mainNumberLessThanSmallestPartition])
+  defaultMain (testGroup "Our Library Tests"
+    [
+      noPartitionFromEmptySet ,
+      mainNumberLessThanSmallestPartition
+    ])
+
+noPartitionFromEmptySet :: TestTree
+noPartitionFromEmptySet = testCase "Testing empty set"
+  (assertEqual "There's no possible partition taking from the empty set"
+  [] (partition 3 []))
 
 mainNumberLessThanSmallestPartition :: TestTree
-mainNumberLessThanSmallestPartition = testCase "Testing "
+mainNumberLessThanSmallestPartition = testCase "Testing small value"
   (assertEqual "There's no possible partition for such small number"
-  [] (partition 3 []))
+  [] (partition 3 [4]))
